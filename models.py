786926f8e55c496490306390e11adc8c6d95c0b4
mamar0 <mattemart94@gmail.com>
2021-06-04 18:58:33 +0200

    some changes

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
@@ -31,8 +31,8 @@ def get_Xy_generator(x, lenght=24, batch_size=1):
 def build_LSTM_model(input_shape = 24):
     #Build the LSTM model
     model = Sequential()
-    model.add(LSTM(128, return_sequences=True, input_shape=(input_shape, 1)))
-    model.add(LSTM(64, return_sequences=False))
+    model.add(LSTM(128, return_sequences=False, input_shape=(input_shape, 1)))
+    #model.add(LSTM(64, return_sequences=False))
     model.add(Dense(25))
     model.add(Dense(1))
 
@@ -42,9 +42,41 @@ def build_LSTM_model(input_shape = 24):
     return model
 
 def evaluate_model(y_true, y_pred):
+    '''
+    Params:
+    -------
+    y_true: numpy.array
+    y_pred: numpy.array
+    
+    Returns:
+    --------
+    None
+    
+    '''
     mse = mean_squared_error(y_true, y_pred)
     score = r2_score(y_true, y_pred)
     mape = mean_absolute_percentage_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
     print(f'mse = {mse}')
-    print(f'mape = {mape}')
     print(f'r2_score = {score}')
+    print(f'mape = {mape}')
+    print(f'mae = {mae}')
+    return None
+    
+def get_forecast_measures(y_true, y_pred):
+    '''
+    Params:
+    -------
+    y_true: numpy.array
+    y_pred: numpy.array
+    
+    Returns:
+    --------
+    d: dict
+    
+    '''
+    d = {}
+    for i in range(1, len(y_true)+1):
+        d[i] = {"mae" : mean_absolute_error(y_true[:i], y_pred[:i]),
+                "mape" : mean_absolute_percentage_error(y_true[:i], y_pred[:i])}
+    return d
